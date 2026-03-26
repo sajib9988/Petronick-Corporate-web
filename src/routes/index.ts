2571e58ea93dcac7d40e587f54956c2c1deaa163
sajib9988 <sajib.biswas9988@gmail.com>
2026-03-26 20:06:24 +0600

    implement promotion agent module with CRUD operations and CSV export

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,17 @@
 import { Router } from "express";
-import { healthRoutes } from "../modules/health/health.route";
+
 import { authRoutes } from "../modules/auth/auth.route";
 import { companyRoutes } from "../modules/company/company.route";
 import { cmsRoutes } from "../modules/cms/cms.route";
 import { contactRoutes } from "../modules/contact/contact.route";
+import { agentRoutes } from "../modules/Promotion-Agent/promotion-agent.route";
 
 const router = Router();
 
-router.use("/health", healthRoutes);
 router.use("/auth", authRoutes);
 router.use("/company", companyRoutes)
 router.use("/cms", cmsRoutes)
 router.use("/contact", contactRoutes)
+router.use("/agents", agentRoutes)
 
 export const apiRoutes = router;
